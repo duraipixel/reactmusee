b2d6fb239d47e7b3fff855e41ef6a844a79b772b
fahdpixel <fahd.pixel@gmail.com>
2023-01-23 18:12:56 +0530

    FEAT: Calendly Integration in home page

--- src/components/LiveVideo.jsx
+++ src/components/LiveVideo.jsx
@@ -1,32 +1,38 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
+import { PopupWidget } from "react-calendly";
 
 export const LiveVideo = () => {
-    return (
-        <Fragment>
-            <section className="live-video">
-                <div className="container">
-                    <div className="row">
-
-                        <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 d-flex justify-content-between align-items-center">
-
-                            <div className="shopping-video">
-                                <div className="common-heads light">
-                                    <h2>Live Video Shopping</h2>
-                                </div>
-                                <div className="secondary-para">
-                                    Connect with us via video call and our experts will help you choose<br /> the perfect musical instrument from the comfort of your home
-                                </div>
-                            </div>
-
-                            <div className="shopping-book">
-                                <a href="">Book Virtual Shopping</a>
-                            </div>
-
-                        </div>
-
-                    </div>
+  return (
+    <Fragment>
+      <section className="live-video">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 d-flex justify-content-between align-items-center">
+              <div className="shopping-video">
+                <div className="common-heads light">
+                  <h2>Live Video Shopping</h2>
                 </div>
-            </section>
-        </Fragment>
-    )
-}
+                <div className="secondary-para">
+                  Connect with us via video call and our experts will help you
+                  choose
+                  <br /> the perfect musical instrument from the comfort of your
+                  home
+                </div>
+              </div>
+
+              <div className="shopping-book">
+                <PopupWidget
+                  url="https://calendly.com/fahd-pixel/class"
+                  rootElement={document.getElementById("root")}
+                  text="Book Video Shopping"
+                  textColor="#ffffff"
+                  color="#00a2ff"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Fragment>
+  );
+};
