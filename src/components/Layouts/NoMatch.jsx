94784185cf1ceb6a3e0c0fc1ab73209386e6f132
fahdpixel <fahd.pixel@gmail.com>
2023-01-24 13:29:32 +0530

    FEAT: Added No match page

--- src/components/Layouts/NoMatch.jsx
+++ src/components/Layouts/NoMatch.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from "react";
 
 export const NoMatch = () => {
   return (
-    <div>NoMatch</div>
-  )
-}
+    <div>
+      <section class="shop-carts">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+              <div class="common-heads text-center">
+                <h2>Oops!</h2>
+              </div>
+            </div>
+            <div class="col-lg-12">
+              <div class="finalcart-list text-center">
+                <img
+                  className="no-match-image"
+                  src="../../assets/illustrations/sigma-1/18.png"
+                  alt="call"
+                />
+                <h3> We can't seem to find the page you're looking for </h3>
+                <br />
+                <div class="load-btn">
+                  <a href="/"> Shop today’s deals </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
