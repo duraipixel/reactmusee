0a4d66925aaf29b1097f06a0d1c1d3004eb787d8
behin-pixel <behin.pixel@gmail.com>
2023-01-30 19:12:21 +0530

    The End

--- src/components/Layouts/MobileFooter.jsx
+++ src/components/Layouts/MobileFooter.jsx
@@ -1,31 +1,58 @@
 import React, { Fragment } from 'react'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { clearAttemptItem } from '../../app/reducer/attemptedCartSlice';
+import { clearCart } from '../../app/reducer/cartSlice';
+import { logoutCustomer } from '../../app/reducer/customerSlice';
 import { isOpenSideBar } from '../../app/reducer/sideMenuBarSlice';
 
 export const MobileFooter = () => {
 
+    const customer = useSelector((state) => state.customer);
     const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const location = useLocation();
 
     const openSideBar = () => {
         dispatch(isOpenSideBar());
     }
 
+    const logout = () => {
+
+        localStorage.removeItem('customer');
+        dispatch(clearCart());
+        dispatch(logoutCustomer());
+        dispatch(clearAttemptItem())
+
+        if (location.pathname == '/cart') {
+            navigate('/');
+        } else {
+            navigate('/login');
+        }
+    }
+
     return (
         <Fragment >
             <div className="mobile-footer">
-                <a className="toggle-button" data-toggle="tooltip" title="Download Your Reports" onClick={openSideBar}>
+                <a className="toggle-button" data-toggle="tooltip" title="All Products" onClick={openSideBar}>
                     <img src="assets/images/mob-ic-1.png" alt="" className="img-fluid" /> Products
                 </a>
-                <a href="" data-toggle="tooltip" title="Current Location">
-                    <img src="assets/images/mob-ic-2.png" alt="" className="img-fluid" /> Login
-                </a>
-                <a href="" data-toggle="tooltip" title="Login to view your Profile">
-                    <img src="assets/images/mob-ic-4.png" alt="" className="img-fluid" /> Compare
-                </a>
-                <a href="" data-toggle="tooltip" title="Cart Details">
+                {
+                    customer.value ? 
+                    <button title="Logout" onClick={() => logout()}>
+                        <img src="assets/images/mob-ic-6.png" alt="" className="img-fluid" /> Logout
+                    </button>
+                    
+                    :
+                    <Link to="/login" data-toggle="tooltip" title="Login">
+                        <img src="assets/images/mob-ic-2.png" alt="" className="img-fluid" /> Login
+                    </Link> 
+
+                }
+                <a href="/cart" data-toggle="tooltip" title="Cart Details">
                     <img src="assets/images/mob-ic-3.png" alt="" className="img-fluid" /> Cart
                 </a>
-                <a href="" target="_blank" data-toggle="tooltip" title="Call Us">
+                <a href="tel:+914428522780" target="_blank" data-toggle="tooltip" title="Call Us">
                     <img src="assets/images/mob-ic-5.png" alt="" className="img-fluid" /> Call Us
                 </a>
             </div>
