8d8e20dc2ec3c74d214fd275ed842a6a6934aef9
duraipixel <durairaj.pixel@gmail.com>
2023-02-09 09:36:19 +0530

    succes page image seting

--- src/components/Filter/FilterPane.jsx
+++ src/components/Filter/FilterPane.jsx
@@ -1,36 +1,40 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import { FilterItems } from './FilterItems';
 import { ProductSkeletonItem } from './../Skeleton/ProductSkeletonItem';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate, createSearchParams, useLocation } from 'react-router-dom';
 import { fetchProducts } from './../../app/reducer/productFilterSlice';
+import { PongSpinner } from 'react-spinners-kit';
+import './filter.css';
 
 export const FilterPane = (props) => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const location = useLocation();
     const searchParams = new URLSearchParams(location.search);
+    const [startLoadMore, setStartLoadMore] = useState(false);
 
     const filterData = useSelector((state) => state.products);
-    
+
     const products = filterData.products != undefined ? filterData.products.products : [];
     // const from = filterData.products.from;
     const to = filterData.products != undefined ? filterData.products.to : 0;
     // const total_count = filterData.products.total_count;
 
     const loadMoreProduct = () => {
-
+        setStartLoadMore(true)
         let page = parseInt(to) / parseInt(window.productLimit);
         const url = new URL(window.location.href);
         const SUrl = "/products/pfilter";
-        
-        searchParams.set("page", page);       
-        navigate(SUrl +'?'+ searchParams.toString());
 
-        dispatch(fetchProducts( '?'+ searchParams.toString()));
-        
+        searchParams.set("page", page);
+        navigate(SUrl + '?' + searchParams.toString());
+
+        dispatch(fetchProducts('?' + searchParams.toString()));
+        setStartLoadMore(false)
+
     }
-    
+
     return (
         <Fragment>
             <div className="col-lg-12 col-md-12 col-sm-12
@@ -56,6 +60,15 @@ export const FilterPane = (props) => {
                             Load More
                         </button>
                     </div>
+                </div>
+                <div className='loadmore-loading'>
+                    <PongSpinner
+                        size={100}
+                        color="#0a1d4a"
+                        loading={startLoadMore}
+                        style={{ top: '50%', left: '45%' }}
+
+                    />
                 </div>
 
             </div>
--- src/pages/PaymentResponse.js
+++ src/pages/PaymentResponse.js
@@ -1,34 +1,39 @@
 import React from 'react'
 import { useParams, Link } from 'react-router-dom';
 import './common.css';
+import { ImagePane } from './../components/Product/ImagePane';
 
 export const PaymentResponse = () => {
 
     const params = useParams();
-    
+
     return (
         <div>
             {
-                params.payment_response == 'success' ? 
-                <div className='row'>
-                    <div className='col-sm-12 text-center'>
-                        <div>
-                            <img src='/assets/illustrations/sigma-1/3.png' className='payment-success-image' alt='Payment success'></img>
+                params.payment_response == 'success' ?
+                    <div className='row'>
+                        <div className='col-sm-12 text-center'>
+                            <div>
+                                <img src='/assets/illustrations/sigma-1/3.png' className='payment-success-image' alt='Payment success'></img>
+                            </div>
                         </div>
-                    </div>
-                    <div className='col-sm-12 text-center mt-5'>
+                        <div className='col-sm-12 text-center mt-5'>
+                            {/* <span className='success-pop-left'>
+                                <img src='/assets/images/success-pop1.gif' />
+                            </span> */}
+                          
                             <h3 className='text-success mb-5'> Payment Success </h3>
                             {/* <button className='mt-3 common-primary-button w-25'>  Track Your Order </button> */}
                             <Link className='mt-8 common-primary-a-tag w-25' to='/profile'> Track Your Order </Link>
-                    </div>
+                        </div>
 
-                </div>
-                :
-                <div>
-                    <h2> Payment Fail </h2>
-                </div>
+                    </div>
+                    :
+                    <div>
+                        <h2> Payment Fail </h2>
+                    </div>
             }
-            
+
         </div>
     )
 }
