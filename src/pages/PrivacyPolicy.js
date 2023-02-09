243191c79116c48720980f2ca028b15f65f55075
behin-pixel <behin.pixel@gmail.com>
2023-02-09 12:29:09 +0530

    Mobile responsive

--- src/pages/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy.js
@@ -3,21 +3,34 @@ import React from 'react'
 export const PrivacyPolicy = () => {
   return (
     <div>
-            <section className="inner-banner shopbybrand">
+            <section className="inner-banner privacypolicy">
                 <div className="container">
                     <div className="row">
                         <div className="inner-liners">
                             <h1>
-                                Your search for the perfect brand 
-                                <br/>
-                                of instruments ends here
+                                Privacy Policy
                             </h1>
                         </div>
                     </div>
                 </div>
             </section>  
-            
-               
+
+            <section className="">
+                <div className="container">
+                    <div className="row">
+                        <div className="contents-sector"> 
+                            <p>Musee Musical respects the privacy of it’s users and is committed to protect it in all respects. This Privacy Policy provides succinctly the manner your data is collected and used by Musee Musical. You are advised to please read the Privacy Policy carefully. By accessing the services provided by Musee Musical you agree to the collection and use of your data by Musee Musical in the manner provided in this Privacy Policy.</p>
+                            <h4>We want you to</h4>
+                            <ul>
+                            <li>feel comfortable using our website.</li>
+                            <li>feel secure and confident while submitting information to us.</li>
+                            <li>be aware that you may contact us with your questions or concerns about privacy on this site.</li>
+                            <li>know that by using our sites you are consenting to the collection of certain data.</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </section>
 
     </div>
   )
