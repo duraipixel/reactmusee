8d6c1e0d85661d23f9168cf48ac26e3d47d65e6d
Fahd <fahd.pixel@gmail.com>
2023-03-06 11:29:36 +0530

    FEAT: Sitemap Implemented

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import ReactDOM from "react-dom/client";
-import App from './App';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'font-awesome/css/font-awesome.min.css';
-import { store } from './app/store'
-import { Provider } from 'react-redux'
-import { BrowserRouter, HashRouter } from "react-router-dom";
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistStore } from 'redux-persist';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import App from "./App";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "font-awesome/css/font-awesome.min.css";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import { HashRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+import { hydrate, render } from "react-dom";
 
 let persistor = persistStore(store);
 
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <HashRouter  >
-          <App />
-        </HashRouter >
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <HashRouter>
+            <App />
+          </HashRouter>
+        </PersistGate>
+      </Provider>
+    </React.StrictMode>,
+    rootElement
+  );
+}
