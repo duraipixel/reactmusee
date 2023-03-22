5de2f7714545b191db220cbff8b3369a78a7c063
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-03-22 11:45:14 +0530

    Packages Support Icon Ui Changes

--- src/components/Home/PackageSupport.jsx
+++ src/components/Home/PackageSupport.jsx
@@ -10,19 +10,19 @@ export const PackageSupport = () => {
                         <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 d-flex justify-content-between">
 
                             <div className="care-box text-left"> 
-                                <span className='text-white'><img width={70} src={require('../../assets/images/customer-service.png')} /> 24x7 <br />Customer Support</span>
+                                <span className='text-white'><img width={50} src={require('../../assets/images/customer-service.png')} /> 24x7 <br />Customer Support</span>
                             </div>
 
                             <div className="care-box text-left">
-                                <span className='text-white'><img width={70} src={require('../../assets/images/free-shipping.png')} /> Free <br />Shipping</span>
+                                <span className='text-white'><img width={70} style={{ transform: 'translate(-20px,-10px)' }} src={require('../../assets/images/free-shipping.png')} /> Free <br />Shipping</span>
                             </div>
 
                             <div className="care-box text-left">
-                                <span className='text-white'><img width={70} src={require('../../assets/images/return.png')} /> Flexible <br />Returns </span>
+                                <span className='text-white'><img width={45} src={require('../../assets/images/return.png')} /> Flexible <br />Returns </span>
                             </div>
 
                             <div className="care-box text-left">
-                                <span className='text-white'><img width={50} src={require('../../assets/images/payment-gateway.png')} /> Secure <br />Payments</span>
+                                <span className='text-white'><img width={40} src={require('../../assets/images/payment-gateway.png')} /> Secure <br />Payments</span>
                             </div>
 
                         </div>
