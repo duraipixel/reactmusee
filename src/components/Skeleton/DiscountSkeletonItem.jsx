049abe61c88c2bda34b6dd079377b5c25bc6f601
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-03-28 12:23:41 +0530

    Deals  Card UI Changes

--- src/components/Skeleton/DiscountSkeletonItem.jsx
+++ src/components/Skeleton/DiscountSkeletonItem.jsx
@@ -1,29 +1,30 @@
 import { Fragment } from 'react'
 import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
-import { Link, useLocation, useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { useDispatch } from 'react-redux';
 import { fetchProducts } from './../../app/reducer/productFilterSlice';
+import { CardActionArea } from '@mui/material';
 
-export const DiscountSkeletonItem = ({discountCollectionData}) => {
+export const DiscountSkeletonItem = ({ discountCollectionData }) => {
 
     const navigate = useNavigate();
     const dispatch = useDispatch();
-    const location  = useLocation();
+    const location = useLocation();
 
     const searchParams = new URLSearchParams(location.search);
 
-    const goToProductListPage = (discount_slug, category_slug = '' ) => {
+    const goToProductListPage = (discount_slug, category_slug = '') => {
         const url = new URL(window.location.href);
         const SUrl = "/products/pfilter";
-        
+
         searchParams.set("discount", discount_slug);
-        if( category_slug ) {
+        if (category_slug) {
             searchParams.set("category", category_slug);
         }
 
-        navigate(SUrl + '?'+ searchParams.toString());
-        dispatch(fetchProducts('?'+ searchParams.toString()));
+        navigate(SUrl + '?' + searchParams.toString());
+        dispatch(fetchProducts('?' + searchParams.toString()));
     }
 
     return (
@@ -32,72 +33,66 @@ export const DiscountSkeletonItem = ({discountCollectionData}) => {
 
                 discountCollectionData.length > 0 ? (
                     <>
-                    {
-                        discountCollectionData.map((item) => (
+                        {
+                            discountCollectionData.map((item) => (
+                                <div className="col-lg-3 col-md-6 col-sm-12 xol-xs-12" key={item.id}>
+                                    <div className="deals-box p-3">
+                                        <h4>
+                                            <label className='m-0' onClick={() => goToProductListPage(item.slug)}>
+                                                {item.collection_name} <span>20% OFF</span>
+                                            </label>
+                                        </h4>
+                                        <div className='row m-0'>
+                                            {
+                                                item.products.length > 0 ? (
 
-                            <div className="col-lg-3 col-md-6 col-sm-12 xol-xs-12" key={item.id}>
+                                                    item.products.map((productItems, i) => (
+                                                        <div className="col-6 p-1">
+                                                            <CardActionArea key={i} className="text-center p-1" onClick={() => goToProductListPage(item.slug, productItems.category_slug)}>
+                                                                <img src={productItems.image} className="deal-image" />
+                                                                <small className='d-block text-start mt-1'>{productItems.category} </small>
+                                                            </CardActionArea>
+                                                        </div>
+                                                    ))
+                                                ) : null
+                                            }
+
+                                        </div>
+                                    </div>
+                                </div>
+                            ))
+                        }
+
+                    </>
+                ) :
+                    Array.from(
+                        { length: 4 },
+                        (_, i) => (
+                            <div className="col-lg-3 col-md-6 col-sm-12 xol-xs-12" key={i}>
                                 <div className="deals-box">
-                                    <h4>
-                                        
-                                        <label role={`button`} onClick={() => goToProductListPage(item.slug)}>
-                                            {item.collection_name} <span>20% OFF</span>
-                                        </label>
-                                        
-                                    </h4>
+                                    <Skeleton />
                                     <ul>
-                                        {
-                                            item.products.length > 0 ? (
-
-                                                item.products.map((productItems, i) => (
-                                                    <li key={i} >
-                                                        <label role={`button`} onClick={() => goToProductListPage(item.slug, productItems.category_slug)}>
-                                                            <img src={productItems.image} />
-                                                            <span>{productItems.category} </span>
-                                                        </label>
-
-                                                    </li>
-                                                    
-                                                ))
-                                                
-                                            ):null
-                                        }
-                                       
+                                        <li>
+                                            <Skeleton width={100} height={100} />
+                                            <Skeleton />
+                                        </li>
+                                        <li>
+                                            <Skeleton width={100} height={100} />
+                                            <Skeleton />
+                                        </li>
+                                        <li>
+                                            <Skeleton width={100} height={100} />
+                                            <Skeleton />
+                                        </li>
+                                        <li>
+                                            <Skeleton width={100} height={100} />
+                                            <Skeleton />
+                                        </li>
                                     </ul>
                                 </div>
                             </div>
-                        ))
-                    }
-                    
-                    </>
-                ) :
-                Array.from(
-                    { length: 4 },
-                    (_, i) => (
-                        <div className="col-lg-3 col-md-6 col-sm-12 xol-xs-12" key={i}>
-                            <div className="deals-box">
-                                <Skeleton />
-                                <ul>
-                                    <li>
-                                        <Skeleton width={100} height={100} />
-                                        <Skeleton />
-                                    </li>
-                                    <li>
-                                        <Skeleton width={100} height={100} />
-                                        <Skeleton />
-                                    </li>
-                                    <li>
-                                        <Skeleton width={100} height={100} />
-                                        <Skeleton />
-                                    </li>
-                                    <li>
-                                        <Skeleton width={100} height={100} />
-                                        <Skeleton />
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
+                        )
                     )
-                )
             }
         </Fragment>
     )
