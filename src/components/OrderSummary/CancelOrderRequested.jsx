9640d88ba23da42d0fa35107188eb6285d7cd67a
Fahd <fahd.pixel@gmail.com>
2023-03-30 16:40:07 +0530

    FEAT: Added Cancel Order Input Radio Reason

--- src/components/OrderSummary/CancelOrderRequested.jsx
+++ src/components/OrderSummary/CancelOrderRequested.jsx
@@ -1,27 +1,45 @@
-import axios from 'axios';
-import { useState } from 'react'
-import { useForm } from 'react-hook-form';
-import { toast } from 'react-toastify';
-import { ErrorMessage } from '@hookform/error-message';
-import Modal from 'react-bootstrap/Modal';
-import { Button, TextField } from '@mui/material';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+import { ErrorMessage } from "@hookform/error-message";
+import Modal from "react-bootstrap/Modal";
+import {
+  Button,
+  FormLabel,
+  FormControl,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+  TextField,
+} from "@mui/material";
 
-export const CancelOrderRequested = ({ orderId, handleCancelRequestShow, cancelShow, handleCancelRequestClose, customer }) => {
-
+export const CancelOrderRequested = ({
+  orderId,
+  handleCancelRequestShow,
+  cancelShow,
+  handleCancelRequestClose,
+  customer,
+}) => {
   const [formLoader, setFormLoader] = useState(false);
+  const [cancelOrderInputRadio, setCancelOrderInputRadio] = useState(false);
+  const [textFieldNotShow, setTextFieldNotShow] = useState(true);
+  const [radioValue, setRadioValue] = useState(false);
 
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
-    reset
+    reset,
   } = useForm();
 
   const onSubmit = (data) => {
     updateCancelOrder(data);
   };
-
+  const handleChange = (e) => {
+    setRadioValue(e.target.value);
+  };
   async function updateCancelOrder(formData) {
 
     setFormLoader(true);
@@ -43,23 +61,79 @@ export const CancelOrderRequested = ({ orderId, handleCancelRequestShow, cancelS
 
   }
 
+  async function CancelOrder() {
+    await axios({
+      url: window.API_URL + "/get/orderCancelReason",
+      method: "get",
+    })
+      .then((res) => {
+        setCancelOrderInputRadio(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  useEffect(() => {
+    CancelOrder();
+  },[]);
+  useEffect(()=>{
+    if (radioValue === "other") {
+      setTextFieldNotShow(false);
+    } else {
+      setTextFieldNotShow(true);
+    }
+  })
   return (
     <Modal
-      className='passwordModal cstmzed'
+      className="passwordModal cstmzed"
       size="md"
       backdrop="static"
       keyboard={false}
       show={cancelShow}
       onHide={handleCancelRequestClose}
     >
-      <Modal.Header closeButton className='bg-light'>
-        <Modal.Title> <h5 className='text-primary fw-bold'>Request Cancel Order</h5> </Modal.Title>
+      <Modal.Header closeButton className="bg-light">
+        <Modal.Title>
+          {" "}
+          <h5 className="text-primary fw-bold">Request Cancel Order</h5>{" "}
+        </Modal.Title>
       </Modal.Header>
-      <form id="cancelOrderForm" onSubmit={handleSubmit(onSubmit)} >
+      <form id="cancelOrderForm" onSubmit={handleSubmit(onSubmit)}>
         <Modal.Body>
           <div className="row">
             <div className="col-lg-12">
+              <FormControl>
+                <FormLabel id="demo-radio-buttons-group-label">
+                  Reason
+                </FormLabel>
+                <RadioGroup
+                  className="mt-1"
+                  onChange={handleChange}
+                  aria-labelledby="demo-radio-buttons-group-label"
+                  defaultValue="none"
+                  name="radio-buttons-group"
+                >
+                  {cancelOrderInputRadio &&
+                    cancelOrderInputRadio.map((item, i) => {
+                      return (
+                        <FormControlLabel
+                          key={i}
+                          value={item.name}
+                          control={<Radio />}
+                          label={item.name}
+                        />
+                      );
+                    })}
+                  <FormControlLabel
+                    value="other"
+                    control={<Radio />}
+                    label="Other"
+                  />
+                </RadioGroup>
+              </FormControl>
               <TextField
+                className="mt-3"
+                hidden={textFieldNotShow}
                 id="outlined-textarea"
                 label="Your comments"
                 placeholder="Type here...."
@@ -71,23 +145,46 @@ export const CancelOrderRequested = ({ orderId, handleCancelRequestShow, cancelS
                 inputProps={{ maxLength: 500 }}
                 {...register("cancelReason", { required: true })}
               />
-              <input type="hidden" {...register("customer_id", { required: "Customer id is required" })} id="customer_id" value={customer && customer.id} />
-              <input type="hidden" {...register("order_id", { required: "Order id is required" })} id="order_id" value={orderId && orderId} />
+              <input
+                type="hidden"
+                {...register("customer_id", {
+                  required: "Customer id is required",
+                })}
+                id="customer_id"
+                value={customer && customer.id}
+              />
+              <input
+                type="hidden"
+                {...register("order_id", { required: "Order id is required" })}
+                id="order_id"
+                value={orderId && orderId}
+              />
             </div>
           </div>
         </Modal.Body>
-        <Modal.Footer className='justify-content-end'>
-          <Button variant="secondary" className='bg-light border me-2' onClick={handleCancelRequestClose}>
+        <Modal.Footer className="justify-content-end">
+          <Button
+            variant="secondary"
+            className="bg-light border me-2"
+            onClick={handleCancelRequestClose}
+          >
             Cancel
           </Button>
-          <Button type="submit" variant="primary" className='btn-dark text-white' disabled={formLoader} >
+          <Button
+            type="submit"
+            variant="primary"
+            className="btn-dark text-white"
+            disabled={formLoader}
+          >
             {formLoader ? (
               <span className="spinner-grow spinner-grow-sm me-1"></span>
-            ) : <i className='bi bi-send-fill me-1'></i>}
+            ) : (
+              <i className="bi bi-send-fill me-1"></i>
+            )}
             Send request
           </Button>
         </Modal.Footer>
       </form>
-    </Modal >
-  )
-}
+    </Modal>
+  );
+};
