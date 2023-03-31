e8aab6da0cc890f9e45c391a94e72dbc6361e279
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-03-31 13:32:13 +0530

    Login Page Ui Changes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,20 +4,19 @@ import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import { useForm } from 'react-hook-form';
-import { ErrorMessage } from '@hookform/error-message';
 import { fetchCarts } from '../app/reducer/cartSlice';
-import { removeAttemptItem } from '../app/reducer/attemptedCartSlice';
 import { loginCustomer } from '../app/reducer/customerSlice';
 import { fetchAddress } from '../app/reducer/customerAddressSlice';
 import { Helmet } from 'react-helmet';
 import { useEffect } from 'react';
+import { Button } from 'rsuite';
+import { FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput } from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
 export const Login = () => {
-
-    const attempt_cart = useSelector((state) => state.attempt_cart);
     const dispatch = useDispatch();
-   
-    // const [localCustomerSession, setLocalCustomerSession] = useState(JSON.parse(window.localStorage.getItem('customer') || null));
     const {
         register,
         handleSubmit,
@@ -53,28 +52,9 @@ export const Login = () => {
 
     const setCustomerAddress = () => {
 
-        let address = JSON.parse(window.localStorage.getItem('addres'));                    
-        dispatch(fetchAddress(address ));
-          
-    }
+        let address = JSON.parse(window.localStorage.getItem('addres'));
+        dispatch(fetchAddress(address));
 
-    async function addCartProduct(item) {
-
-        let customer = JSON.parse(window.localStorage.getItem('customer'));
-        const res_data = { ...item, customer_id: customer.id, quantity: 1 };
-
-        await axios({
-            url: window.API_URL + '/add/cart',
-            method: 'POST',
-            data: res_data,
-        }).then((res) => {
-
-            localStorage.setItem('cart', JSON.stringify(res.data));
-            dispatch(fetchCarts(JSON.parse(window.localStorage.getItem('cart'))))
-
-        }).catch((err) => {
-
-        })
     }
 
     async function getSiteInformation() {
@@ -84,12 +64,11 @@ export const Login = () => {
         }).then((res) => {
             localStorage.setItem('site_info', JSON.stringify(res.data));
         }).catch((err) => {
-
         })
     }
 
     async function doLoginCustomer(formData) {
-        const form_data = {...formData, guest_token: localStorage.getItem('guest_token') || ''}
+        const form_data = { ...formData, guest_token: localStorage.getItem('guest_token') || '' }
         setLoginFormLoader(true);
         axios({
             url: window.API_URL + '/login',
@@ -104,130 +83,105 @@ export const Login = () => {
                 reset();
 
             } else {
-
-                toast.success( window.LOGIN_SUCCESS_MSG);
-
+                toast.success(window.LOGIN_SUCCESS_MSG);
                 if (res.data.customer_data) {
                     localStorage.removeItem('guest_token');
                     localStorage.setItem('customer', JSON.stringify(res.data.customer_data))
-
                     dispatch(loginCustomer(JSON.parse(window.localStorage.getItem('customer'))));
-                 
-                    localStorage.setItem('address', JSON.stringify(res.data.customer_data.customer_address))               
-                   
+                    localStorage.setItem('address', JSON.stringify(res.data.customer_data.customer_address))
                     fetchCartProducts();
-                    setCustomerAddress()
-                    // console.log('attempt_cart');
-                    // if (attempt_cart.attempt_cart.length > 0) {
-                    //     console.log('attempt_cart inner loop');
-                    //     addCartProduct(attempt_cart.attempt_cart);
-                    //     dispatch(removeAttemptItem(attempt_cart.attempt_cart))
-                    // }
-                    // console.log('attempt_cart ouerside loop');
-
+                    setCustomerAddress() 
                     getSiteInformation();
                 }
-                console.log('pofile');
-                // navigate('/profile');
                 navigate(-1);
-
             }
-            
         }).catch((err) => {
-
         })
     }
-
     useEffect(() => {
-      if ( window.localStorage.getItem('customer') ) {
-        navigate('/');
-      }
+        if (window.localStorage.getItem('customer')) {
+            navigate('/');
+        }
     }, [])
-    
 
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
     return (
         <Fragment>
             <Helmet>
                 <title>Login | Musee Musical</title>
                 <link rel="canonical" href={window.location.href} />
-                <meta name='description' content='login page'/>
+                <meta name='description' content='login page' />
             </Helmet>
-            <section className="tab-of-sectors lgon-pge">
-                <div className="container">
-                    <div className="row justify-content-center">
-                        <div className="col-lg-9 col-md-9 col-sm-9">
-                            <div className="row fully-bxn g-0">
-                                <div className="col-lg-6">
-                                    <div className="dhoni-bgm">
-                                        <div className="common-heading">
-                                            <h2>
-                                                Welcome to<br /> <span>Musee Musical</span>
-                                            </h2>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-lg-6">
-                                    <div className="cir-frm">
-                                        <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
-                                            <div className="frm-fields row clearfix">
-                                                <div className="col-lg-12 col-md-12 col-sm-12">
-                                                    <div className="common-heading">
-                                                        <h2>
-                                                            Login <span> Here! </span>
-                                                        </h2>
-                                                    </div>
-                                                    <div className="row">
-                                                        <div className="form-data col-lg-12 mb-3">
-                                                            <input className="form-control" type="text" {...register("email", {
-                                                                required: "Email is required", pattern: {
-                                                                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                                                                    message: "Invalid email address"
-                                                                }
-                                                            })} placeholder="Email" />
-                                                            <ErrorMessage errors={errors} name="email" as="p" />
-                                                        </div>
-                                                        <div className="form-data col-lg-12 mb-3">
-                                                            <input className="form-control" type="password" {...register("password", { required: "Password is required", maxLength: 20 })} placeholder="Password" />
-                                                            <ErrorMessage errors={errors} name="password" as="p" />
-                                                        </div>
-                                                        <div className='mb-3 text-end user-register'>
-                                                            <Link to='/forgotpassword' style={{ color: '#313190', fontWeight: '800' }}> Forgot Password? </Link>
-                                                        </div>
-                                                        <div className="form-data sbm col-lg-12 mb-3">
-                                                            <button type='submit' disabled={loginFormLoader} >
-                                                                {loginFormLoader && (
-                                                                    <span className="spinner-grow spinner-grow-sm"></span>
-                                                                )} Sign In
-                                                            </button>
-
-                                                        </div>
-                                                    </div>
-                                                    {/* <div className="col-lg-12 text-center p-0">
-                                                        <div className="mid-poart">
-                                                            <h5>or</h5>
-                                                        </div>
-                                                    </div>
-                                                    <div className="col-lg-12 text-center p-0">
-                                                        <div className="login-btn">
-                                                            <span> Login with <a href=""><img src="/assets/images/google.png" /></a> <a href=""><img src="/assets/images/facebook.png" /></a> </span>
-                                                        </div>
-                                                    </div> */}
-                                                    <div className="col-lg-12 text-center mt-3">
-                                                        <div className="user-regster">
-                                                            Don't have an account?
-                                                            <Link to="/register"> Register Here! </Link>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </form>
-                                    </div>
+            <section className='d-flex align-items-center justify-content-center p-2' style={{
+                background: 'linear-gradient(rgb(0 0 0 / 46%), rgb(0 0 0 / 67%)), url(https://cdn.pixabay.com/photo/2013/11/03/08/05/cheers-204742_960_720.jpg)',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center center',
+                minHeight: '80vh'
+            }}>
+                <div className='card shadow-lg border col-md-4 rounded-4'>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <CardContent className='p-4'>
+                            <Typography gutterBottom variant="h5" component="div" className='mb-4 text-uppercase text-primary fw-bold text-center'>
+                                Login Here!
+                            </Typography>
+                            <FormControl fullWidth variant="outlined" className='mb-4'>
+                                <InputLabel error={errors.email ? true : false} htmlFor="outlined-adornment-email">Email</InputLabel>
+                                <OutlinedInput
+                                    id="outlined-adornment-email"
+                                    type='email'
+                                    label="Email"
+                                    error={errors.email ? true : false}
+                                    {...register("email", {
+                                        required: true, pattern: {
+                                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                            message: "Invalid email address"
+                                        }
+                                    })}
+                                />
+                            </FormControl>
+                            <FormControl fullWidth variant="outlined">
+                                <InputLabel error={errors.password ? true : false} htmlFor="outlined-adornment-password">Password</InputLabel>
+                                <OutlinedInput
+                                    id="outlined-adornment-password"
+                                    type={showPassword ? 'text' : 'password'}
+                                    endAdornment={
+                                        <InputAdornment position="end">
+                                            <IconButton
+                                                aria-label="toggle password visibility"
+                                                onClick={handleClickShowPassword}
+                                                onMouseDown={handleMouseDownPassword}
+                                                edge="end"
+                                            >
+                                                {showPassword ? <VisibilityOff /> : <Visibility />}
+                                            </IconButton>
+                                        </InputAdornment>
+                                    }
+                                    {...register("password", { required:true, maxLength: 20 })}
+                                    error={errors.password ? true : false}
+                                    label="Password"
+                                />
+                            </FormControl>
+                            <Link to='/forgotpassword' className='text-primary mb-3 mt-2 float-end'> Forgot Password? </Link>
+                            <Button type='submit' loading={loginFormLoader} size="lg" className='py-3 btn-dark text-white text-uppercase w-100' >
+                                <i class="bi bi-music-note-beamed me-2"></i>
+                                Sign In
+                            </Button>
+                            <div className="col-lg-12 text-center mt-3">
+                                <div className="user-regster">
+                                    Don't have an account?
+                                    <Link to="/register"> Register Here! </Link>
                                 </div>
                             </div>
-                        </div>
-                    </div>
+                        </CardContent>
+                    </form>
                 </div>
-            </section>
-        </Fragment>
+            </section> 
+        </Fragment >
     )
 }
