e0a030b80e352a1218fc739aabc0c51572ee6bb1
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-04-06 12:46:07 +0530

    Search bar mobile view UI fixing

--- src/components/Layouts/SearchMobile.jsx
+++ src/components/Layouts/SearchMobile.jsx
@@ -1,15 +1,33 @@
 import { Fragment, useState } from 'react'
 import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
 import { MagicSpinner } from 'react-spinners-kit';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
-export const SearchMobile = ({searchShow, handleSearchModalClose}) => {
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import ListItemText from '@mui/material/ListItemText';
+import ListItem from '@mui/material/ListItem';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import CloseIcon from '@mui/icons-material/Close';
+import Slide from '@mui/material/Slide';
+import { forwardRef } from 'react';
+
+const Transition = forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export const SearchMobile = ({ searchShow, handleSearchModalClose }) => {
 
     const [searchStart, setSearchStart] = useState(false);
     const [searchData, setSearchData] = useState([]);
-    
+    const navigate = useNavigate()
+
     const globalSearch = (event) => {
         setSearchStart(true)
         var search_type = 'product';
@@ -30,88 +48,100 @@ export const SearchMobile = ({searchShow, handleSearchModalClose}) => {
         }).catch((err) => {
         })
     }
-
+    const showProduct = (url) => {
+        navigate(`/product/${url}`)
+        handleSearchModalClose()
+    }
     return (
-        <Modal className='cstmzed' show={searchShow} onHide={handleSearchModalClose}>
-
-            <Fragment>
-                <Modal.Header closeButton>
-                    <Modal.Title id='addressFormTitle'>Search your Products</Modal.Title>
-                </Modal.Header>
-
-                <Modal.Body>
-                    <div className="modal-body">
-                        <div className="top-search">
-                            <div className="form-data" id='mobile_search_pane'>
-                                <input type="search" placeholder="Search..." onChange={globalSearch} />
-                                <ul className="src-fndings" id='searchPane'>
-
-                                            {searchData.length > 0 ? searchData.map((item, i) => (
-                                                <li key={i}>
-                                                    {
-                                                        item.has_data === 'yes' ?
-                                                            item.product_name ?
-                                                                <Link to={`/product/${item.product_url}`} className="w-100" >
-                                                                    <div className='w-100 m-flex'>
-                                                                        <div className='w-20'>
-                                                                            <img src={item.image} width="75" />
-                                                                        </div>
-                                                                        <div className='w-80'>
-                                                                            <label htmlFor="">
-                                                                                {item.product_name}
-                                                                            </label>
-                                                                            <span>
-                                                                                Home | {item.parent_category_name} | {item.category_name} | {item.brand_name} | {item.product_name}
-                                                                            </span>
-                                                                        </div>
-                                                                    </div>
-
-                                                                </Link>
-                                                                :
-                                                                <Link to='/'>
-                                                                    <img src={item.image} width="100" /> {item.product_name}
-                                                                    <span>
-                                                                        Home | {item.parent_category_name} | {item.category_name} | {item.brand_name} | {item.product_name}
-                                                                    </span>
-                                                                </Link>
-
-                                                            :
-                                                            <div className='w-100' style={{ textAlign: 'center' }}> {item.message}</div>
-                                                    }
-                                                </li>
-
-                                            ))
-                                                :
-
-                                                
-                                                    !searchStart ? 
-                                                    (
-                                                    <div className='w-100' style={{ textAlign: 'center' }}> No records found</div>
-                                                    )
-
-                                                    :
-                                                    (<li style={{ height: 'inherit' }}>
-                                                        <div id="product-loader" className='w-100' >
-                                                            <div className='product-wrapper'>
-                                                                <MagicSpinner
-                                                                    size={100}
-                                                                    color="#0a1d4a"
-                                                                    loading={true}
-                                                                    style={{ top: '50%', left: '45%' }}
-                                                                />
-                                                            </div>
+        <>
+            <Dialog
+                fullScreen
+                open={searchShow}
+                onClose={handleSearchModalClose}
+                TransitionComponent={Transition}
+            >
+                <AppBar sx={{ position: 'relative' }}>
+                    <Toolbar>
+                        <Typography sx={{ flex: 1 }} variant="h6" component="div">
+                            <img src="/assets/images/logo.svg" width={80} />
+                        </Typography>
+                        <IconButton
+                            edge="start"
+                            color="inherit"
+                            onClick={handleSearchModalClose}
+                            aria-label="close"
+                        >
+                            <CloseIcon />
+                        </IconButton>
+                    </Toolbar>
+                </AppBar>
+                <div className='modal-body p-3'>
+                    <div id='mobile_search_pane'>
+                        <strong>Search</strong>
+                        <input type="search" className='form-control form-control-sm mt-2' placeholder="Search for products, brands and more ..." onChange={globalSearch} />
+                        <ul className="list-group mt-3" style={{ height: '70vh', overflow: 'auto' }}>
+                            {searchData.length > 0 ? searchData.map((item, i) => (
+                                <li key={i} className='list-group-item list-group-item-action'>
+                                    {
+                                        item.has_data === 'yes' ?
+                                            item.product_name ?
+                                                <div onClick={() => showProduct(item.product_url)} className="w-100 text-dark" >
+                                                    <div className='row'>
+                                                        <div className='col-3'>
+                                                            <img src={item.image} height={100} width={100} />
                                                         </div>
-                                                    </li>)
-                                                
-                                            }
+                                                        <div className='col ps-0'>
+                                                            <label className='fw-bold'>
+                                                                {item.product_name}
+                                                            </label>
+                                                            <span className='small'>
+                                                                Home | {item.parent_category_name} | {item.category_name} | {item.brand_name} | {item.product_name}
+                                                            </span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                :
+                                                <Link to='/'>
+                                                    <img src={item.image} width="100" /> {item.product_name}
+                                                    <span>
+                                                        Home | {item.parent_category_name} | {item.category_name} | {item.brand_name} | {item.product_name}
+                                                    </span>
+                                                </Link>
 
-                                        </ul>
-                            </div>
-                        </div>
+                                            :
+                                            <div className='w-100' style={{ textAlign: 'center' }}> {item.message}</div>
+                                    }
+                                </li>
+
+                            ))
+                                :
+
+
+                                !searchStart ?
+                                    (
+                                        <div className='w-100' style={{ textAlign: 'center' }}> No records found</div>
+                                    )
+
+                                    :
+                                    (<li style={{ height: 'inherit' }}>
+                                        <div id="product-loader" className='w-100' >
+                                            <div className='product-wrapper'>
+                                                <MagicSpinner
+                                                    size={100}
+                                                    color="#0a1d4a"
+                                                    loading={true}
+                                                    style={{ top: '50%', left: '45%' }}
+                                                />
+                                            </div>
+                                        </div>
+                                    </li>)
+
+                            }
+
+                        </ul>
                     </div>
-                </Modal.Body>
-            </Fragment>
-
-        </Modal >
+                </div>
+            </Dialog>
+        </>
     )
 }
