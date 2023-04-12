f813c47704a3ac0de356561e3d793e641d57b852
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-04-12 18:38:03 +0530

    Pre Loader Function Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
+import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './app/constant.js';
 import { Layout } from './components/Layouts/Layout.jsx';
-import { NoMatch } from './components/Layouts/NoMatch.jsx';
-import Home from './pages/Home';
+import { NoMatch } from './components/Layouts/NoMatch.jsx'; 
 import { ProductDetail } from './pages/ProductDetail.js';
 import { Collection } from './pages/Collection';
 import { Category } from './pages/Category.js';
-import { ShopByBrand } from './pages/ShopByBrand'; 
+import { ShopByBrand } from './pages/ShopByBrand';
 import { Cart } from './pages/Cart';
 import { Login } from './pages/Login';
 import { Register } from './pages/Register';
@@ -28,6 +28,7 @@ import "rsuite/dist/rsuite.min.css";
 import 'rsuite/styles/index.less';
 import './assets/css/custom.css';
 import { VerifyAccount } from './components/Profile/VerifyAccount';
+import PreLoader from './components/PreLoader'
 
 const theme = createTheme({
   palette: {
@@ -38,7 +39,7 @@ const theme = createTheme({
       main: '#eeeeee',
     },
     secondary: {
-      main:'#313190'
+      main: '#313190'
     },
     error: {
       main: '#b60000',
@@ -51,34 +52,38 @@ const theme = createTheme({
     },
   },
 });
+const HomeComponent = lazy(() => import('./pages/Home.js'));
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path='product/:product_url' element={<ProductDetail />} />
-          <Route path='category/:category_slug' element={<Category />} />
-          <Route path='products/:collection_slug' element={<Collection />} />
-          <Route path='brand' element={<ShopByBrand />} />
-          <Route path='brands/:brand_slug' element={<BrandDetails />} />
-          <Route path='cart' element={<Cart />} />
-          <Route path='login' element={<Login />} />
-          <Route path='register' element={<Register />} />
-          <Route path='forgotpassword' element={<ForgotPassword />} />
-          <Route path='reset-password/:token' element={<ResetPassword />} />
-          <Route path='profile' element={<Profile />} />
-          <Route path='ordersummary/:order_no' element={<OrderSummary />} />
-          <Route path='thankyou/:payment_response' element={<PaymentResponse />} />
-          <Route path='privacypolicy' element={<PrivacyPolicy />} />
-          <Route path='termsofuse' element={<TermsofUse />} />
-          <Route path='returnpolicy' element={<ReturnPolicy />} />
-          <Route path='shippingpolicy' element={<ShippingPolicy />} />
-          <Route path='verify-account/:token' element={<VerifyAccount />} />
-          <Route path="*" element={<NoMatch />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<PreLoader/>}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomeComponent />} />
+            <Route path='product/:product_url' element={<ProductDetail />} />
+            <Route path='category/:category_slug' element={<Category />} />
+            <Route path='products/:collection_slug' element={<Collection />} />
+            <Route path='brand' element={<ShopByBrand />} />
+            <Route path='brands/:brand_slug' element={<BrandDetails />} />
+            <Route path='cart' element={<Cart />} />
+            <Route path='login' element={<Login />} />
+            <Route path='register' element={<Register />} />
+            <Route path='forgotpassword' element={<ForgotPassword />} />
+            <Route path='reset-password/:token' element={<ResetPassword />} />
+            <Route path='profile' element={<Profile />} />
+            <Route path='ordersummary/:order_no' element={<OrderSummary />} />
+            <Route path='thankyou/:payment_response' element={<PaymentResponse />} />
+            <Route path='privacypolicy' element={<PrivacyPolicy />} />
+            <Route path='termsofuse' element={<TermsofUse />} />
+            <Route path='returnpolicy' element={<ReturnPolicy />} />
+            <Route path='shippingpolicy' element={<ShippingPolicy />} />
+            <Route path='verify-account/:token' element={<VerifyAccount />} />
+            <Route path="*" element={<NoMatch />} />
+          </Route>
+        </Routes>
+      </Suspense>
+
       <ToastContainer position="top-center"
         autoClose={1000}
         hideProgressBar
