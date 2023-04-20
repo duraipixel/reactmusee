6ca2e213711dc778b0523e886e5284b8d7d2f971
duraipixel <durairaj.pixel@gmail.com>
2023-04-20 13:19:00 +0530

    cache menu top menu added

--- src/app/services/topMenuApi.js
+++ src/app/services/topMenuApi.js
@@ -3,9 +3,19 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const topMenuApi = createApi({
     reducerPath: 'topMenuApi',
     baseQuery: fetchBaseQuery({ baseUrl:window.API_URL}),
+    tagTypes: ['topMenuApi'],
     endpoints: (builder) => ({
         topMenu: builder.query({
-            query:() => '/get/topMenu'
+            query:() => '/get/topMenu',
+            providesTags: ['topMenuApi']
+        }),
+        increase: builder.mutation({
+            query: ({ ing }) => {
+              return {
+                url: `/get/topMenu`,
+                method: "PATCH",                
+              };
+            },
         })
     })
 })
